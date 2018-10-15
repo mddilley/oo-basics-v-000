ed8f0c8dffa3d7d59afaeee8924c9f5cc57cef1d
Mike Dilley <mike.dilley@gmail.com>
2018-10-15 20:34:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,35 +3,35 @@ class Book
   # def initialize(title)
   #   @title = title
   # end
-  # 
+  #
   # def title
   #   @title
   # end
-  # 
+  #
   # def author=(author)
   #   @author = author
   # end
-  # 
+  #
   # def author
   #   @author
   # end
-  # 
+  #
   # def page_count=(num)
   #   @page_count = num
   # end
-  # 
+  #
   # def page_count
   #   @page_count
   # end
-  # 
+  #
   # def genre=(genre)
   #   @genre = genre
   # end
-  # 
+  #
   # def genre
   #   @genre
   # end
-  # 
+  #
   # def turn_page
   #   puts "Flipping the page...wow, you read fast!"
   # end
@@ -41,6 +41,6 @@ class Book
 
   def initialize(title)
     @title = title
-  end  
+  end
 
 end
