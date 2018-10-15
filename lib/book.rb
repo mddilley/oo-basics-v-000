2c1c46b4d709d03a79440819317f151f0f8ddd0f
Mike Dilley <mike.dilley@gmail.com>
2018-10-15 20:35:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -32,15 +32,17 @@ class Book
   #   @genre
   # end
   #
-  # def turn_page
-  #   puts "Flipping the page...wow, you read fast!"
-  # end
+
 
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
   def initialize(title)
     @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 
 end
